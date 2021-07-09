fe7102fab329f05ec65a55c3a4037fdab6d7d11e
StormMaster99 <74252852+StormMaster99@users.noreply.github.com>
2021-07-09 16:18:22 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBkflPDy7ah5TWcpEUj3uaflV-7-7JR3EU",
+    authDomain: "message-website-897da.firebaseapp.com",
+    databaseURL: "https://message-website-897da-default-rtdb.firebaseio.com",
+    projectId: "message-website-897da",
+    storageBucket: "message-website-897da.appspot.com",
+    messagingSenderId: "218002691133",
+    appId: "1:218002691133:web:57b752c959ec8319884654",
+    measurementId: "G-PET0SKXZE6"
+  };
+// Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  function addroom(){
+    
+  }
