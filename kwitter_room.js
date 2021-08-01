d9caa3319991afc5e2185b81971b8c816fee4ff9
StormMaster99 <74252852+StormMaster99@users.noreply.github.com>
2021-08-01 11:43:22 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -10,6 +10,30 @@ var firebaseConfig = {
   };
 // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  u=localStorage.getItem("Username"); 
+ document.getElementById("wel").innerHTML="Welcome, "+u+"!";
+
   function addroom(){
-    
+    Roomname= document.getElementById("rn").value;
+    console.log(Roomname);
+    firebase.database().ref("/").child(Roomname).update({
+          Purpose: "Adding a room name"
+    });
+    localStorage.setItem("Room_name",Roomname);
+    window.location="kwitter_page.html";
   }
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//Start code
+console.log(Room_names)
+row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >Room Name : "+ Room_names +"</div><hr>";
+document.getElementById("output").innerHTML +=row;
+//End code
+});});}
+getData();
+function redirectToRoomName(r){
+  console.log(r);
+  localStorage.setItem("Room_name",r);
+  window.location="kwitter_page.html";
+}
+
